2776482e8e026952f5ea646f5d378ff198cd4291
Faisal Bhat <plesim18@gmail.com>
2024-05-09 08:40:07 +0000

    Editor test 3

--- flame-frontend/src/components/Editor/Editor.jsx
+++ flame-frontend/src/components/Editor/Editor.jsx
@@ -5,12 +5,12 @@ export const Editor = ()=>{
         // this is a test editor for the first time
     <div className='tc ma2 pa2 shadow-2'>
 
-        <iframe
-        frameBorder="0"
-        height="450px"  
-        src="https://onecompiler.com/embed/" 
-        width="100%"
-        ></iframe>
+<iframe 
+  frameBorder="0" 
+  height="700px" 
+  width="100%"
+  src="https://onecompiler.com/embed"
+></iframe>
     </div>
     )
 }
