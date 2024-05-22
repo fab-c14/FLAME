a8216a5f3bfea8710f7a3ae6872915f57e36e2d5
Faisal Bhat <plesim18@gmail.com>
2024-05-22 07:40:54 +0000

    updated the chats

--- flame-frontend/src/Pages/Profile/UserStatsChart.jsx
+++ flame-frontend/src/Pages/Profile/UserStatsChart.jsx
@@ -1,44 +1,38 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
-import { Line } from 'react-chartjs-2';
-import { Chart as ChartJS, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
 
-ChartJS.register(LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 const UserStatsChart = ({ selectedStudent }) => {
   // Replace the sample data with actual student progress data
   const studentProgress = selectedStudent; // Assuming you have this data
 
   const data = {
+    labels: ['Total Runs', 'Successful Runs', 'Failed Runs'],
     datasets: [
       {
-        label: 'Total Runs',
-        data: studentProgress.totalRuns || 0, // Replace with actual total runs data
-        fill: false,
-        backgroundColor: 'rgb(75, 192, 192)',
-        borderColor: 'rgba(75, 192, 192, 0.2)',
-      },
-      {
-        label: 'Successful Runs',
-        data: studentProgress.successfulRuns || [24, 32, 43, 2],
-        fill: false,
-        backgroundColor: 'rgb(153, 102, 255)',
-        borderColor: 'rgba(153, 102, 255, 0.2)',
-      },
-      {
-        label: 'Failed Runs',
-        data: studentProgress.failedRuns || 0,
-        fill: false,
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgba(255, 99, 132, 0.2)',
-      },
+        label: 'Runs',
+        
+        data: [
+          studentProgress.totalRuns || 45, 
+          studentProgress.successfulRuns || 24, 
+          studentProgress.failedRuns || 2
+        ],
+        backgroundColor: [
+          'rgb(75, 192, 192)',
+          'rgb(153, 102, 255)',
+          'rgb(255, 99, 132)'
+        ],
+      }
     ],
   };
 
   const options = {
     scales: {
       x: {
-        display: false, // Hide x-axis labels (weeks)
+        beginAtZero: true,
       },
       y: {
         beginAtZero: true,
@@ -50,7 +44,7 @@ const UserStatsChart = ({ selectedStudent }) => {
     <Card className="br3 shadow-2 mt4">
       <Card.Body>
         <Card.Title className="tc">Progress of {selectedStudent.name}</Card.Title>
-        <Line data={data} options={options} />
+        <Bar data={data} options={options} />
       </Card.Body>
     </Card>
   );
