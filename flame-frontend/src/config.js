123058b1e28b163386b522b24f73ad518324fcec
Faisal Bhat <plesim18@gmail.com>
2024-07-20 16:41:08 +0000

    update

--- flame-frontend/src/config.js
+++ flame-frontend/src/config.js
@@ -1,3 +1,3 @@
-export const BACKEND_URL="https://5000-fabc14-flame-k5jg1wytmqf.ws-us115.gitpod.io";
+export const BACKEND_URL="https://flame-api-bmf9.onrender.com";
 export const PORT = 5000;
 // this is because we will be using the url many times in our program that will create difficulty
