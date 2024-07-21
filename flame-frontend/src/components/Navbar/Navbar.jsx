739c9af53d88b48f41803af706944c3921d0b10b
Faisal Bhat <plesim18@gmail.com>
2024-07-21 08:23:09 +0000

    final updates

--- flame-frontend/src/components/Navbar/Navbar.jsx
+++ flame-frontend/src/components/Navbar/Navbar.jsx
@@ -8,20 +8,20 @@ const CustomNavbar = ({isLoggedIn}) => {
 
     
     return (
-        <Navbar expand="lg" className="py-3 black shadow-3 ma3 br2 bw1 b pa2 ba  hover-navbar b--black">
+        <Navbar expand="lg" className="py-3 black shadow-3 ma3 br2 bw1 b pa2 ba  hover-navbar b--black ">
             <Navbar.Brand href="" className="mr-auto ml4 ma2">
                 <div>
-                    <img src={logo} alt="Logo" height="45" className="d-inline-block align-top br2 b--black" />
+                    <img src={logo} alt="Logo" height="45" className="d-inline-block align-top br2 b--black dim" />
                 </div>
             </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav pa2 ma2" />
+            <Navbar.Toggle aria-controls="basic-navbar-nav pa2 ma2 glow" />
             <Navbar.Collapse id="basic-navbar-nav ma2 pa2 py-3">
                 <Nav className="ml-auto">
-                    <Link to="/" className="nav-link hover-bg-light-red ma2 pa2 br2">Home</Link>
-                    <Link to="/about" className="nav-link hover-bg-light-red ma2 pa2 br2">About</Link>
-                    <Link to="/docs" className="nav-link hover-bg-light-red ma2 pa2 br2">Docs</Link>
+                    <Link to="/" className="nav-link hover-bg-light-red ma2 pa2 br2 grow">Home</Link>
+                    <Link to="/about" className="nav-link hover-bg-light-red ma2 pa2 br2 grow">About</Link>
+                    <Link to="/docs" className="nav-link hover-bg-light-red ma2 pa2 br2 grow">Docs</Link>
                 
-                    <Link to={isLoggedIn ? `/Profile` : `/login`} className="nav-link hover-bg-light-red ma2 pa2 br2 d-flex align-items-center">
+                    <Link to={isLoggedIn ? `/Profile` : `/login`} className="nav-link hover-bg-light-red ma2 pa2 br2 d-flex align-items-center grow">
                             {isLoggedIn? <><FiUser className="mr2" /> Profile</> : "Login" }
                     </Link>
                 </Nav>
